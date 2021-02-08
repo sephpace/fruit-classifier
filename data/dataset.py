77513b10c426183615033192bdd781c8ea134bf3
seph <sephpace@gmail.com>
2021-02-08 15:14:18 -0700

    Add option to load all dataset into RAM.

--- data/dataset.py
+++ data/dataset.py
@@ -18,21 +18,24 @@ class Fruits360Dataset(Dataset):
     The Fruits 360 dataset.
 
     Attributes:
-        data (list of tuple): The paths to each image in the dataset and its
-                              associated label.
+        data (list of tuple): The images / paths to images in the dataset and their
+                              associated labels.
         labels (list of str): Labels of fruit types.
     """
 
-    def __init__(self, test=False):
+    def __init__(self, test=False, load_all=False):
         """
         Constructor.
 
         Parameters:
-            test (bool): Will use the testing dataset if True and the training
-                         dataset if False.
+            test (bool):     Will use the testing dataset if True and the training
+                                 dataset if False.
+            load_all (bool): Will load the entire dataset into RAM to increase speed if True.
         """
+        self.__load_all = load_all
+
         # Distinguish between testing and training data
-        if(test):
+        if test:
             data_dir = f'{ROOT_DIR}/data/fruits-360/Testing'
         else:
             data_dir = f'{ROOT_DIR}/data/fruits-360/Training'
@@ -48,7 +51,10 @@ class Fruits360Dataset(Dataset):
                 if os.path.isfile(path):
                     label = path.split('/')[-2]
                     target = self.labels.index(label)
-                    self.data.append((path, target))
+                    if self.__load_all:
+                        self.data.append((self.__load_image(path), target))
+                    else:
+                        self.data.append((path, target))
 
     def __getitem__(self, idx):
         """
@@ -60,11 +66,12 @@ class Fruits360Dataset(Dataset):
         Returns:
             (Tensor): An image tensor.
         """
-        path, label = self.data[idx]
-        img = Image.open(path)
-        tensor = torch.as_tensor(np.asarray(img)).float() / 255
-        tensor = tensor.transpose(1, 2).transpose(0, 1)
-        return tensor, label
+        if self.__load_all:
+            return self.data[idx]
+        else:
+            path, label = self.data[idx]
+            tensor = self.__load_image(path)
+            return tensor, label
     
     def __len__(self):
         """
@@ -74,3 +81,19 @@ class Fruits360Dataset(Dataset):
             (int): The length of the dataset.
         """
         return len(self.data) 
+
+    @staticmethod
+    def __load_image(path):
+        """
+        Loads the image at the given path and converts it into a tensor.
+
+        Parameters:
+            path (str): The path to the image file.
+
+        Returns:
+            (Tensor): The image tensor.
+        """
+        img = Image.open(path)
+        tensor = torch.as_tensor(np.asarray(img).copy()).float() / 255
+        tensor = tensor.transpose(1, 2).transpose(0, 1)
+        return tensor
