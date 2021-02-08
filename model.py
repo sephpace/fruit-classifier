80713fa3f1c68d8fa3fc44e6d2c5569a02a0ca77
seph <sephpace@gmail.com>
2021-02-08 15:17:39 -0700

    Add dropout to model.

--- model.py
+++ model.py
@@ -3,7 +3,6 @@ Author: Seph Pace
 Email:  sephpace@gmail.com
 """
 
-import torch
 from torch import nn
 from torch.nn import functional as F
 
@@ -23,18 +22,19 @@ class FruitClassifier(nn.Module):
         out (Sequential):  The output layers.
     """
 
-    def __init__(self, class_count):
+    def __init__(self, class_count, drop_p=0.1):
         """
         Constructor.
 
         Parameters:
             class_count (int): The amount of classes.
+            drop_p (float):    The dropout probability.
         """
         super().__init__()
         self.class_count = class_count
 
         # Set up convolutional layers
-        conv = (
+        convolutional_layers = (
             nn.Conv2d(3, 6, 5),
             nn.Conv2d(6, 9, 5),
             nn.Conv2d(9, 12, 5),
@@ -42,22 +42,26 @@ class FruitClassifier(nn.Module):
 
         # Combine all modules into sequential
         modules = []
-        for c in conv:
-            modules.append(c)
+        for conv in convolutional_layers:
+            modules.append(conv)
             modules.append(nn.MaxPool2d(2))
             modules.append(nn.ReLU())
         self.main = nn.Sequential(*modules)
 
+        # Set up dropout for conv layers
+        self.conv_drop = nn.Dropout2d(drop_p)
+
         # Set up output layers
-        lin = (
+        linear_layers = (
             nn.Linear(972, 486),
             nn.Linear(486, 243),
             nn.Linear(243, self.class_count),
         )
         modules = []
-        for l in lin:
-            modules.append(l)
+        for lin in linear_layers:
+            modules.append(lin)
             modules.append(nn.ReLU())
+            modules.append(nn.Dropout(drop_p))
         self.out = nn.Sequential(*modules)
 
     def forward(self, x):
@@ -71,6 +75,7 @@ class FruitClassifier(nn.Module):
             (Tensor): A softmax output of size (N, C).
         """
         y = self.main(x)
+        y = self.conv_drop(y)
         y = y.flatten(start_dim=1)
         y = self.out(y)
         return F.log_softmax(y, dim=1)
